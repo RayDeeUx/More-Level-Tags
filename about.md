8cf859779d772f24b5bc0bb577d80f904f13c3c0
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-04-12 22:51:06 -0400

    stable?

--- about.md
+++ about.md
@@ -1,8 +1,17 @@
-# More Level Tags
+# Even More Level Tags
 
-Adds <cl>more</c> tags next to the level creator name to signify different things.
-This <cr>does not</c> show in the level browser, only on the level page. This is because of a technical limitation in <cg>GD</c>.
+Adds <cl>even more</c> tags next to the level author's name in a level's info layer to symbolize a level's traits.
+
+Disclaimer: Due to various technical limitations, not all levels will recieve this treatment.
 
 Tags and their meanings:
-- ![Ship icon](frame:portal_04_extra_2_001.png&scale:1) | Level uses legacy upside down ship physics
-- ![Cube icon](frame:portal_03_extra_2_001.png&scale:1) | Level uses 2P mode
+- ![Ship icon](frame:portal_04_extra_2_001.png&scale:0.85) | Level uses legacy upside down ship physics
+- ![Robot icon](frame:portal_14_extra_2_001.png&scale:0.85) | Level uses legacy robot jump pad physics
+- ![Flip icon](frame:portal_02_extra_2_001.png&scale:0.85) | Level starts with gravity flipped
+- ![Height icon](frame:portal_19_extra_2_001.png&scale:0.85) | Level disables dynamic level height
+- ![Rotate trigger icon](frame:edit_eRotateComBtn_001.png&scale:0.35) | Level disables multi rotation
+- ![Swing icon](frame:portal_18_extra_2_001.png&scale:0.85) | Level disables 2.2's physics changes
+- ![Scale trigger icon](frame:edit_eScaleComBtn_001.png&scale:0.35) | Level disables the negative scale fix
+- ![Cube icon](frame:portal_03_extra_2_001.png&scale:0.85) | Level uses 2P mode
+
+If a level's tags ever appear to be in the wrong position, simply exit and re-enter the level's info layer.
--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,93 @@ using namespace geode::prelude;
 CCMenu* menu;
 LoadingCircle* loading;
 CCNode* l;
+CCNode* r;
+CCNode* f;
+CCNode* h;
+CCNode* m;
+CCNode* n;
+CCNode* t;
 std::vector<CCNode*> buttons;
+bool isRefreshing = false;
+
+void hideTags(std::string decomp) {
+	if (buttons.size() < 1) return; 
+	if (decomp.size() > 1) {
+		if ((l != nullptr) && decomp.find("kA32,0")) {
+			l->setVisible(decomp.find("kA32,0") != std::string::npos);
+			if (!l->isVisible())
+			{
+				l->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), l), buttons.end());
+			}
+		}
+		if ((r != nullptr) && decomp.find("kA34,0")) {
+			r->setVisible(decomp.find("kA34,0") != std::string::npos);
+			if (!r->isVisible())
+			{
+				r->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), r), buttons.end());
+			}
+		}
+		if ((f != nullptr) && decomp.find("kA11,1")) {
+			f->setVisible(decomp.find("kA11,1") != std::string::npos);
+			if (!f->isVisible())
+			{
+				f->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), f), buttons.end());
+			}
+		}
+		if ((h != nullptr) && decomp.find("kA37,0")) {
+			h->setVisible(decomp.find("kA37,0") != std::string::npos);
+			if (!h->isVisible())
+			{
+				h->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), h), buttons.end());
+			}
+		}
+		if ((m != nullptr) && decomp.find("kA27,0")) {
+			m->setVisible(decomp.find("kA27,0") != std::string::npos);
+			if (!m->isVisible())
+			{
+				m->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), m), buttons.end());
+			}
+		}
+		if ((t != nullptr) && decomp.find("kA40,0")) {
+			t->setVisible(decomp.find("kA40,0") != std::string::npos);
+			if (!t->isVisible())
+			{
+				t->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), t), buttons.end());
+			}
+		}
+		if ((n != nullptr) && decomp.find("kA33,0")) {
+			n->setVisible(decomp.find("kA33,0") != std::string::npos);
+			if (!n->isVisible())
+			{
+				n->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), n), buttons.end());
+			}
+		}
+	}
+}
 
 class $modify(LevelInfoLayer)
 {
+	void onUpdate(cocos2d::CCObject* sender) {
+		isRefreshing = true;
+		LevelInfoLayer::onUpdate(sender);
+	}
 	bool init(GJGameLevel* p0, bool p1)
 	{
+		isRefreshing = false;
 		buttons.clear();
 
-		if (!LevelInfoLayer::init(p0, p1))
-			return false;
+		if (!LevelInfoLayer::init(p0, p1)) { return false; }
 
 		menu = CCMenu::create();
 		menu->setContentSize(ccp(0, 0));
-		menu->setID("tags-menu"_spr);
+		menu->setID("more-level-tags-menu"_spr);
 		menu->setScale(0.15f);
 		menu->setPosition(as<CCMenu*>(this->getChildByID("creator-info-menu"))->getPosition() + ccp(as<CCMenu*>(this->getChildByID("creator-info-menu"))->getScaledContentSize().width / 2 + 7 + (this->getChildByID("copy-indicator") ? 18 : 0) + (this->getChildByID("high-object-indicator") ? 18 : 0), 7 + 1.6f) + ccp(5, 0));
 
@@ -32,23 +105,60 @@ class $modify(LevelInfoLayer)
 
 		auto ship = CCSprite::createWithSpriteFrameName("portal_04_extra_2_001.png");
 		ship->setScale(3.5f);
-
-		l = CircleButtonSprite::create(ship, CircleBaseColor::Cyan, CircleBaseSize::Large); // CCSprite::createWithSpriteFrameName("GJ_dislikesIcon_001.png");
-		l->setID("legacy-ship");
+		l = CircleButtonSprite::create(ship, CircleBaseColor::Cyan, CircleBaseSize::Large);
+		l->setID("legacy-ship"_spr);
 		buttons.push_back(l);
 		menu->addChild(l);
+
+		auto robot = CCSprite::createWithSpriteFrameName("portal_14_extra_2_001.png");
+		robot->setScale(3.5f);
+		r = CircleButtonSprite::create(robot, CircleBaseColor::Pink, CircleBaseSize::Large);
+		r->setID("legacy-robot"_spr);
+		buttons.push_back(r);
+		menu->addChild(r);
+
+		auto flip = CCSprite::createWithSpriteFrameName("portal_02_extra_2_001.png");
+		flip->setScale(3.5f);
+		f = CircleButtonSprite::create(flip, CircleBaseColor::Pink, CircleBaseSize::Large);
+		f->setID("flip-gravity"_spr);
+		buttons.push_back(f);
+		menu->addChild(f);
+
+		auto height = CCSprite::createWithSpriteFrameName("portal_19_extra_2_001.png");
+		height->setScale(3.5f);
+		h = CircleButtonSprite::create(height, CircleBaseColor::Gray, CircleBaseSize::Large);
+		h->setID("dynamic-height"_spr);
+		buttons.push_back(h);
+		menu->addChild(h);
+
+		auto rotate = CCSprite::createWithSpriteFrameName("edit_eRotateComBtn_001.png");
+		rotate->setScale(1.5f);
+		m = CircleButtonSprite::create(rotate, CircleBaseColor::Gray, CircleBaseSize::Large);
+		m->setID("multi-rotate"_spr);
+		buttons.push_back(m);
+		menu->addChild(m);
+
+		auto tpt = CCSprite::createWithSpriteFrameName("portal_18_extra_2_001.png");
+		tpt->setScale(3.5f);
+		t = CircleButtonSprite::create(tpt, CircleBaseColor::Gray, CircleBaseSize::Large);
+		t->setID("two-point-two"_spr);
+		buttons.push_back(t);
+		menu->addChild(t);
+
+		auto negScale = CCSprite::createWithSpriteFrameName("edit_eScaleComBtn_001.png");
+		negScale->setScale(1.5f);
+		n = CircleButtonSprite::create(negScale, CircleBaseColor::Gray, CircleBaseSize::Large);
+		n->setID("negative-scale"_spr);
+		buttons.push_back(n);
+		menu->addChild(n);
 
 		if (p0->m_twoPlayerMode)
 		{
 			auto cube = CCSprite::createWithSpriteFrameName("portal_03_extra_2_001.png");
-			//auto cube = CCSprite::createWithSpriteFrameName("GJ_dislikesIcon_001.png");
 			cube->setScale(3.5f);
-
 			auto p2 = CircleButtonSprite::create(cube, CircleBaseColor::Pink, CircleBaseSize::Large);
-			p2->setID("2-player");
-
+			p2->setID("two-player"_spr);
 			menu->addChild(p2);
-			
 			buttons.push_back(p2);
 		}
 
@@ -56,24 +166,36 @@ class $modify(LevelInfoLayer)
 
 		if (p0->m_levelString.size() > 1)
 		{
-			auto pl = PlayLayer::create(p0, true, true);
-			CCDirector::get()->getOpenGLView()->showCursor(true);
 
-			l->setVisible(!pl->m_levelSettings->m_propertykA32);
-
-			loading->removeFromParent();
-			if (!l->isVisible())
-			{
-				l->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), l), buttons.end());
+			if (loading) { loading->removeFromParent(); }
+			std::string decomp = ZipUtils::decompressString(p0->m_levelString.c_str(), true, 0);
+			hideTags(decomp);
+		} else {
+			if (l) {
+				buttons.push_back(l);
+				l->setVisible(false);
 			}
-		}
-		else
-		{
-			buttons.push_back(l);
-			
-			l->setVisible(false);
-			loading->setVisible(true);
+			if (r) {
+				buttons.push_back(r);
+				r->setVisible(false);
+			}
+			if (f) {
+				buttons.push_back(f);
+				r->setVisible(false);
+			}
+			if (h) {
+				buttons.push_back(h);
+				r->setVisible(false);
+			}
+			if (m) {
+				buttons.push_back(m);
+				r->setVisible(false);
+			}
+			if (t) {
+				buttons.push_back(t);
+				r->setVisible(false);
+			}
+			if (loading) { loading->setVisible(true); }
 		}
 
 		for (size_t i = 0; i < buttons.size(); i++)
@@ -85,40 +207,29 @@ class $modify(LevelInfoLayer)
 		return true;
 	}
 
-	virtual TodoReturn levelDownloadFinished(GJGameLevel* p0)
+	virtual void levelDownloadFinished(GJGameLevel* p0)
 	{
 		LevelInfoLayer::levelDownloadFinished(p0);
-
-		log::info("levelDownloadFinished");
 
 		if (menu)
 		{
 			if (p0->m_levelString.size() > 1)
 			{
-				auto pl = PlayLayer::create(p0, true, true);
-				CCDirector::get()->getOpenGLView()->showCursor(true);
-				delete pl;
-
-				if (l)
-					l->setVisible(!pl->m_levelSettings->m_propertykA32);
-				if (loading)
-					loading->setVisible(false);
-
-				if (l)
-				{
-					if (!l->isVisible())
-					{
-						l->removeFromParent();
-						buttons.erase(std::remove(buttons.begin(), buttons.end(), l), buttons.end());
-					}
+				if (loading) { loading->setVisible(false); }
+				if (!isRefreshing) {
+					std::string decomp = ZipUtils::decompressString(p0->m_levelString.c_str(), true, 0);
+					hideTags(decomp);
 				}
 			}
 			else
 			{
-				if (l)
-					l->setVisible(false);
-				if (loading)
-					loading->setVisible(true);
+				if (l) { l->setVisible(false); }
+				if (r) { r->setVisible(false); }
+				if (f) { f->setVisible(false); }
+				if (h) { h->setVisible(false); }
+				if (m) { m->setVisible(false); }
+				if (t) { t->setVisible(false); }
+				if (loading) { loading->setVisible(true); }
 			}
 
 			for (size_t i = 0; i < buttons.size(); i++)
@@ -127,5 +238,6 @@ class $modify(LevelInfoLayer)
 			}
 
 		}
+		isRefreshing = false;
 	}
 };
