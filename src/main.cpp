4abed4f7fe2925753fe6d70ba4bb8d974099e661
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-05-23 22:54:15 -0400

    how much did i break with this one? im on macos so i can't test

--- src/main.cpp
+++ src/main.cpp
@@ -1,160 +1,163 @@
-#include <Geode/Geode.hpp>
 #include <Geode/modify/LevelInfoLayer.hpp>
 #include <Geode/modify/GJGameLevel.hpp>
 
 using namespace geode::prelude;
-
-CCMenu* menu;
-CCNode* l;
-CCNode* r;
-CCNode* f;
-CCNode* h;
-CCNode* m;
-CCNode* n;
-CCNode* t;
-std::vector<CCNode*> buttons;
-bool isRefreshing = false;
 
 /*
 	consent to continue TheSillyDoggo's More Level Tags mod is below:
 	https://discord.com/channels/911701438269386882/911702535373475870/1228540436277366794
 */
 
-void hideTags(std::string decomp) {
-	if (buttons.size() < 1) return; 
-	if (decomp.size() > 1) {
-		if ((l != nullptr) && decomp.find("kA32,0")) {
-			l->setVisible(decomp.find("kA32,0") != std::string::npos);
-			if (!l->isVisible())
-			{
-				l->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), l), buttons.end());
+class $modify(LevelInfoLayer) {
+	struct Fields {
+		CCMenu* menu = nullptr;
+		CCNode* l = nullptr;
+		CCNode* r = nullptr;
+		CCNode* f = nullptr;
+		CCNode* h = nullptr;
+		CCNode* m = nullptr;
+		CCNode* n = nullptr;
+		CCNode* t = nullptr;
+		std::vector<CCNode*> buttons = nullptr;
+		bool isRefreshing = false;
+	};
+	void hideTags(std::string decomp) {
+		if (buttons.size() < 1) return; 
+		if (decomp.size() > 1) {
+			if ((m_fields->l != nullptr) && decomp.find("kA32,0")) {
+				m_fields->l->setVisible(decomp.find("kA32,0") != std::string::npos);
+				if (!m_fields->l->isVisible())
+				{
+					m_fields->l->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), l), buttons.end());
+				}
 			}
-		}
-		if ((r != nullptr) && decomp.find("kA34,0")) {
-			r->setVisible(decomp.find("kA34,0") != std::string::npos);
-			if (!r->isVisible())
-			{
-				r->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), r), buttons.end());
+			if ((m_fields->r != nullptr) && decomp.find("kA34,0")) {
+				m_fields->r->setVisible(decomp.find("kA34,0") != std::string::npos);
+				if (!m_fields->r->isVisible())
+				{
+					m_fields->r->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), r), buttons.end());
+				}
 			}
-		}
-		if ((f != nullptr) && decomp.find("kA11,1")) {
-			f->setVisible(decomp.find("kA11,1") != std::string::npos);
-			if (!f->isVisible())
-			{
-				f->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), f), buttons.end());
+			if ((m_fields->f != nullptr) && decomp.find("kA11,1")) {
+				m_fields->f->setVisible(decomp.find("kA11,1") != std::string::npos);
+				if (!m_fields->f->isVisible())
+				{
+					m_fields->f->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), f), buttons.end());
+				}
 			}
-		}
-		if ((h != nullptr) && decomp.find("kA37,0")) {
-			h->setVisible(decomp.find("kA37,0") != std::string::npos);
-			if (!h->isVisible())
-			{
-				h->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), h), buttons.end());
+			if ((m_fields->h != nullptr) && decomp.find("kA37,0")) {
+				m_fields->h->setVisible(decomp.find("kA37,0") != std::string::npos);
+				if (!m_fields->h->isVisible())
+				{
+					m_fields->h->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), h), buttons.end());
+				}
 			}
-		}
-		if ((m != nullptr) && decomp.find("kA27,0")) {
-			m->setVisible(decomp.find("kA27,0") != std::string::npos);
-			if (!m->isVisible())
-			{
-				m->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), m), buttons.end());
+			if ((m_fields->m != nullptr) && decomp.find("kA27,0")) {
+				m_fields->m->setVisible(decomp.find("kA27,0") != std::string::npos);
+				if (!m_fields->m->isVisible())
+				{
+					m_fields->m->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), m), buttons.end());
+				}
 			}
-		}
-		if ((t != nullptr) && decomp.find("kA40,0")) {
-			t->setVisible(decomp.find("kA40,0") != std::string::npos);
-			if (!t->isVisible())
-			{
-				t->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), t), buttons.end());
+			if ((m_fields->t != nullptr) && decomp.find("kA40,0")) {
+				m_fields->t->setVisible(decomp.find("kA40,0") != std::string::npos);
+				if (!m_fields->t->isVisible())
+				{
+					m_fields->t->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), t), buttons.end());
+				}
 			}
-		}
-		if ((n != nullptr) && decomp.find("kA33,0")) {
-			n->setVisible(decomp.find("kA33,0") != std::string::npos);
-			if (!n->isVisible())
-			{
-				n->removeFromParent();
-				buttons.erase(std::remove(buttons.begin(), buttons.end(), n), buttons.end());
+			if ((m_fields->n != nullptr) && decomp.find("kA33,0")) {
+				m_fields->n->setVisible(decomp.find("kA33,0") != std::string::npos);
+				if (!m_fields->n->isVisible())
+				{
+					m_fields->n->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), n), buttons.end());
+				}
 			}
 		}
 	}
-}
-
-class $modify(LevelInfoLayer)
-{
 	void onUpdate(cocos2d::CCObject* sender) {
-		isRefreshing = true;
+		m_fields->isRefreshing = true;
 		LevelInfoLayer::onUpdate(sender);
 	}
 	void onPlay(cocos2d::CCObject* sender) {
-		isRefreshing = true;
+		m_fields->isRefreshing = true;
 		LevelInfoLayer::onPlay(sender);
 	}
 	bool init(GJGameLevel* p0, bool p1)
 	{
-		isRefreshing = false;
-		buttons.clear();
+		m_fields->isRefreshing = false;
+		m_fields->buttons.clear();
 
 		if (!LevelInfoLayer::init(p0, p1)) { return false; }
 
 		if (p0->m_accountID.value() == 71 && !getChildByIDRecursive("right-side-menu")->isVisible()) { return true; } // avoid false positives with robtop's levels
 
-		menu = CCMenu::create();
-		menu->setContentSize(ccp(0, 0));
-		menu->setID("more-level-tags-menu"_spr);
-		menu->setScale(0.15f);
-		menu->setPosition(as<CCMenu*>(this->getChildByID("creator-info-menu"))->getPosition() + ccp(as<CCMenu*>(this->getChildByID("creator-info-menu"))->getScaledContentSize().width / 2 + 7 + (this->getChildByID("copy-indicator") ? 18 : 0) + (this->getChildByID("high-object-indicator") ? 18 : 0), 7 + 1.6f) + ccp(5, 0));
+		auto creatorInfoMenu = getChildByID("creator-info-menu");
+		if (!creatorInfoMenu) { return true; }
+
+		m_fields->menu = CCMenu::create();
+		m_fields->menu->setContentSize(ccp(0, 0));
+		m_fields->menu->setID("more-level-tags-menu"_spr);
+		m_fields->menu->setScale(0.15f);
+		m_fields->menu->setPosition(as<CCMenu*>(creatorInfoMenu)->getPosition() + ccp(as<CCMenu*>(creatorInfoMenu)->getScaledContentSize().width / 2 + 7 + (getChildByID("copy-indicator") ? 18 : 0) + (getChildByID("high-object-indicator") ? 18 : 0), 7 + 1.6f) + ccp(5, 0));
+		m_fields->menu->setVisible(false);
+		addChild(m_fields->menu);
 
 		auto ship = CCSprite::createWithSpriteFrameName("portal_04_extra_2_001.png");
 		ship->setScale(3.5f);
-		l = CircleButtonSprite::create(ship, CircleBaseColor::Cyan, CircleBaseSize::Large);
-		l->setID("legacy-ship"_spr);
-		buttons.push_back(l);
-		menu->addChild(l);
+		m_fields->l = CircleButtonSprite::create(ship, CircleBaseColor::Cyan, CircleBaseSize::Large);
+		m_fields->l->setID("legacy-ship"_spr);
+		m_fields->buttons.push_back(m_fields->l);
+		m_fields->menu->addChild(m_fields->l);
 
 		auto robot = CCSprite::createWithSpriteFrameName("portal_14_extra_2_001.png");
 		robot->setScale(3.5f);
-		r = CircleButtonSprite::create(robot, CircleBaseColor::Pink, CircleBaseSize::Large);
-		r->setID("legacy-robot"_spr);
-		buttons.push_back(r);
-		menu->addChild(r);
+		m_fields->r = CircleButtonSprite::create(robot, CircleBaseColor::Pink, CircleBaseSize::Large);
+		m_fields->r->setID("legacy-robot"_spr);
+		m_fields->buttons.push_back(m_fields->r);
+		m_fields->menu->addChild(m_fields->r);
 
 		auto flip = CCSprite::createWithSpriteFrameName("portal_02_extra_2_001.png");
 		flip->setScale(3.5f);
-		f = CircleButtonSprite::create(flip, CircleBaseColor::Pink, CircleBaseSize::Large);
-		f->setID("flip-gravity"_spr);
-		buttons.push_back(f);
-		menu->addChild(f);
+		m_fields->f = CircleButtonSprite::create(flip, CircleBaseColor::Pink, CircleBaseSize::Large);
+		m_fields->f->setID("flip-gravity"_spr);
+		m_fields->buttons.push_back(m_fields->f);
+		m_fields->menu->addChild(m_fields->f);
 
 		auto height = CCSprite::createWithSpriteFrameName("portal_19_extra_2_001.png");
 		height->setScale(3.5f);
-		h = CircleButtonSprite::create(height, CircleBaseColor::Gray, CircleBaseSize::Large);
-		h->setID("dynamic-height"_spr);
-		buttons.push_back(h);
-		menu->addChild(h);
+		m_fields->h = CircleButtonSprite::create(height, CircleBaseColor::Gray, CircleBaseSize::Large);
+		m_fields->h->setID("dynamic-height"_spr);
+		m_fields->buttons.push_back(m_fields->h);
+		m_fields->menu->addChild(m_fields->h);
 
 		auto rotate = CCSprite::createWithSpriteFrameName("edit_eRotateComBtn_001.png");
 		rotate->setScale(1.5f);
-		m = CircleButtonSprite::create(rotate, CircleBaseColor::Gray, CircleBaseSize::Large);
-		m->setID("multi-rotate"_spr);
-		buttons.push_back(m);
-		menu->addChild(m);
+		m_fields->m = CircleButtonSprite::create(rotate, CircleBaseColor::Gray, CircleBaseSize::Large);
+		m_fields->m->setID("multi-rotate"_spr);
+		m_fields->buttons.push_back(m_fields->m);
+		m_fields->menu->addChild(m_fields->m);
 
 		auto tpt = CCSprite::createWithSpriteFrameName("portal_18_extra_2_001.png");
 		tpt->setScale(3.5f);
-		t = CircleButtonSprite::create(tpt, CircleBaseColor::Gray, CircleBaseSize::Large);
-		t->setID("two-point-two"_spr);
-		buttons.push_back(t);
-		menu->addChild(t);
+		m_fields->t = CircleButtonSprite::create(tpt, CircleBaseColor::Gray, CircleBaseSize::Large);
+		m_fields->t->setID("two-point-two"_spr);
+		m_fields->buttons.push_back(m_fields->t);
+		m_fields->menu->addChild(m_fields->t);
 
 		auto negScale = CCSprite::createWithSpriteFrameName("edit_eScaleComBtn_001.png");
 		negScale->setScale(1.5f);
-		n = CircleButtonSprite::create(negScale, CircleBaseColor::Gray, CircleBaseSize::Large);
-		n->setID("negative-scale"_spr);
-		buttons.push_back(n);
-		menu->addChild(n);
+		m_fields->n = CircleButtonSprite::create(negScale, CircleBaseColor::Gray, CircleBaseSize::Large);
+		m_fields->n->setID("negative-scale"_spr);
+		m_fields->buttons.push_back(m_fields->n);
+		m_fields->menu->addChild(m_fields->n);
 
 		if (p0->m_twoPlayerMode)
 		{
@@ -162,48 +165,45 @@ class $modify(LevelInfoLayer)
 			cube->setScale(3.5f);
 			auto p2 = CircleButtonSprite::create(cube, CircleBaseColor::Pink, CircleBaseSize::Large);
 			p2->setID("two-player"_spr);
-			menu->addChild(p2);
-			buttons.push_back(p2);
+			m_fields->menu->addChild(p2);
+			m_fields->buttons.push_back(p2);
 		}
 
-		this->addChild(menu);
-
-		if (p0->m_levelString.size() > 1)
-		{
+		if (p0->m_levelString.size() > 1) {
 			std::string decomp = ZipUtils::decompressString(p0->m_levelString.c_str(), true, 0);
 			hideTags(decomp);
 		} else {
-			if (l) {
-				buttons.push_back(l);
-				l->setVisible(false);
+			if (m_fields->l) {
+				m_fields->buttons.push_back(m_fields->l);
+				m_fields->l->setVisible(false);
 			}
-			if (r) {
-				buttons.push_back(r);
-				r->setVisible(false);
+			if (m_fields->r) {
+				m_fields->buttons.push_back(m_fields->r);
+				m_fields->r->setVisible(false);
 			}
-			if (f) {
-				buttons.push_back(f);
-				r->setVisible(false);
+			if (m_fields->f) {
+				m_fields->buttons.push_back(m_fields->f);
+				m_fields->r->setVisible(false);
 			}
-			if (h) {
-				buttons.push_back(h);
-				r->setVisible(false);
+			if (m_fields->h) {
+				m_fields->buttons.push_back(m_fields->h);
+				m_fields->r->setVisible(false);
 			}
-			if (m) {
-				buttons.push_back(m);
-				r->setVisible(false);
+			if (m_fields->m) {
+				m_fields->buttons.push_back(m_fields->m);
+				m_fields->r->setVisible(false);
 			}
-			if (t) {
-				buttons.push_back(t);
-				r->setVisible(false);
+			if (m_fields->t) {
+				m_fields->buttons.push_back(m_fields->t);
+				m_fields->r->setVisible(false);
 			}
 		}
 
-		for (size_t i = 0; i < buttons.size(); i++)
-		{
-			buttons[i]->setPositionX((18 / 0.15f) * i);
+		for (size_t i = 0; i < m_fields->buttons.size(); i++) {
+			m_fields->buttons[i]->setPositionX((18 / 0.15f) * i);
 		}
-		
+
+		m_fields->menu->setVisible(true);
 
 		return true;
 	}
@@ -212,32 +212,31 @@ class $modify(LevelInfoLayer)
 	{
 		LevelInfoLayer::levelDownloadFinished(p0);
 
-		if (menu)
-		{
-			if (p0->m_levelString.size() > 1)
-			{
-				if (!isRefreshing) {
+		if (m_fields->menu) {
+			if (p0->m_levelString.size() > 1) {
+				if (!m_fields->isRefreshing) {
+					m_fields->menu->setVisible(false);
 					std::string decomp = ZipUtils::decompressString(p0->m_levelString.c_str(), true, 0);
 					hideTags(decomp);
 				}
-			}
-			else
-			{
-				if (l) { l->setVisible(false); }
-				if (r) { r->setVisible(false); }
-				if (f) { f->setVisible(false); }
-				if (h) { h->setVisible(false); }
-				if (m) { m->setVisible(false); }
-				if (t) { t->setVisible(false); }
+			} else {
+				if (m_fields->l) { m_fields->l->setVisible(false); }
+				if (m_fields->r) { m_fields->r->setVisible(false); }
+				if (m_fields->f) { m_fields->f->setVisible(false); }
+				if (m_fields->h) { m_fields->h->setVisible(false); }
+				if (m_fields->m) { m_fields->m->setVisible(false); }
+				if (m_fields->t) { m_fields->t->setVisible(false); }
 			}
 
-			for (size_t i = 0; i < buttons.size(); i++)
-			{
-				buttons[i]->setPositionX((18 / 0.15f) * i);
+			for (size_t i = 0; i < buttons.size(); i++) {
+				m_fields->buttons[i]->setPositionX((18 / 0.15f) * i);
 			}
 			
-			menu->setScale(0.15f);
-			menu->setPosition(as<CCMenu*>(this->getChildByID("creator-info-menu"))->getPosition() + ccp(as<CCMenu*>(this->getChildByID("creator-info-menu"))->getScaledContentSize().width / 2 + 7 + (this->getChildByID("copy-indicator") ? 18 : 0) + (this->getChildByID("high-object-indicator") ? 18 : 0), 7 + 1.6f) + ccp(5, 0));
+			if (const auto creatorInfoMenu = getChildByID("creator-info-menu")) {
+				m_fields->menu->setScale(0.15f);
+				m_fields->menu->setPosition(as<CCMenu*>(creatorInfoMenu)->getPosition() + ccp(as<CCMenu*>(creatorInfoMenu)->getScaledContentSize().width / 2 + 7 + (getChildByID("copy-indicator") ? 18 : 0) + (getChildByID("high-object-indicator") ? 18 : 0), 7 + 1.6f) + ccp(5, 0));
+				m_fields->menu->setVisible(true);
+			}
 		}
 		isRefreshing = false;
 	}
