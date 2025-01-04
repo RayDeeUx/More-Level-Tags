151cb228f5cacd071dcd6883ebad23750f0293f5
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-01-03 20:45:38 -0500

    frontfacing changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/FLAlertLayer.hpp>
 #include <regex>
 
 #define getInt Mod::get()->getSettingValue<int64_t>
@@ -228,5 +229,20 @@ class $modify(MyLevelInfoLayer, LevelInfoLayer) {
 		m_fields->menu->setVisible(true);
 
 		m_fields->isRefreshing = false;
+	}
+};
+
+class $modify(MyFLAlertLayer, FLAlertLayer) {
+	static void onModify(auto & self)
+	{
+		(void) self.setHookPriority("FLAlertLayer::init", -2133456789);
+	}
+	bool init(FLAlertLayerProtocol* delegate, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
+		const std::string& titleAsString = title;
+		const std::string& descAsString = desc;
+		if (!utils::string::contains(titleAsString, " Trigger Intolerance") && !utils::string::contains(titleAsString, "Camera ") && !utils::string::contains(titleAsString, "Shader ") && !utils::string::startsWith(descAsString, "When determining if a level should display the ") && !utils::string::contains(descAsString, "EverAfter by Galofuf") && !utils::string::contains(descAsString, "HIGHLY DISCOURAGED") && !utils::string::contains(descAsString, "and should be reserved for the likes of NASA supercomputers.")) {
+			return FLAlertLayer::init(delegate, title, desc, btn1, btn2, width, scroll, height, textScale);
+		}
+		return FLAlertLayer::init(delegate, title, desc, btn1, btn2, 420.f, true, 320.f, 1.0f);
 	}
 };
